f243212148566ca2ec3d07e1d9dfdbdcbf777f3c
Serge Ratke <serge.ratke@gmail.com>
2020-05-29 22:19:29 +0200

    ausdrücke

---
+++ bin/build.bat
@@ -0,0 +1,20 @@
+@echo off
+
+set compiler_flags=-Od -D_CRT_SECURE_NO_WARNINGS -MTd -nologo -fp:fast -fp:except- -Gm- -GR- -EHa- -Zo -Oi -WX -W4 -wd4244 -wd4201 -wd4100 -wd4214 -wd4101 -wd4189 -wd4505 -wd4127 -wd4706 -wd4702 -wd4204 -FC -Z7 -I%PROJECT_PATH%/src -I"D:\Dev\mysql-8.0.19-winx64\include"
+set linker_flags= -incremental:no -opt:ref -libpath:"D:\Dev\mysql-8.0.19-winx64\lib" Ws2_32.lib Secur32.lib libmysql.lib
+
+IF NOT exist %BUILD_PATH% ( mkdir %BUILD_PATH% )
+pushd %BUILD_PATH%
+
+cl %compiler_flags%               ^
+    %PROJECT_PATH%\src\chunk.c    ^
+    %PROJECT_PATH%\src\compiler.c ^
+    %PROJECT_PATH%\src\debug.c    ^
+    %PROJECT_PATH%\src\memory.c   ^
+    %PROJECT_PATH%\src\scanner.c  ^
+    %PROJECT_PATH%\src\value.c    ^
+    %PROJECT_PATH%\src\vm.c       ^
+    %PROJECT_PATH%\src\main.c     ^
+    -Fe%PROJECT_NAME%.exe /link %linker_flags%
+
+popd
