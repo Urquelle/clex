40ba5c8674b41527b26634eba697d28a8202ad9b
Serge Ratke <serge.ratke@gmail.com>
2020-05-30 13:49:47 +0200

    hashtable

--- bin/build.bat
+++ bin/build.bat
@@ -10,12 +10,13 @@ cl %compiler_flags%               ^
     %PROJECT_PATH%\src\chunk.c    ^
     %PROJECT_PATH%\src\compiler.c ^
     %PROJECT_PATH%\src\debug.c    ^
+    %PROJECT_PATH%\src\main.c     ^
     %PROJECT_PATH%\src\memory.c   ^
     %PROJECT_PATH%\src\object.c   ^
     %PROJECT_PATH%\src\scanner.c  ^
+    %PROJECT_PATH%\src\table.c    ^
     %PROJECT_PATH%\src\value.c    ^
     %PROJECT_PATH%\src\vm.c       ^
-    %PROJECT_PATH%\src\main.c     ^
     -Fe%PROJECT_NAME%.exe /link %linker_flags%
 
 popd
